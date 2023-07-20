15638890a7cbe7a64041cf04367477128cb1ae39
amanitka <ballnazzar@gmail.com>
2023-07-20 21:41:54 +0200

    Fix host for flask server

--- swarmkeeper.py
+++ swarmkeeper.py
@@ -38,4 +38,4 @@ scheduler.start()
 atexit.register(close_scheduler)
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5090, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=5090, debug=False, threaded=True)
