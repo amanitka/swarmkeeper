635d87bf3681df1dcd52920fe97d6c442a82aaec
amanitka <ballnazzar@gmail.com>
2023-07-21 22:24:10 +0200

    Add support for exclusion via service labels

--- docker_service/docker_api.py
+++ docker_service/docker_api.py
@@ -1,5 +1,8 @@
 import docker
 import logging
+
+from docker.models.services import Service
+
 from config.config import config
 
 
@@ -10,6 +13,17 @@ class DockerApi:
         logging.info(f"Initialize docker client with base url: {docker_base_url}")
         self.__client = docker.DockerClient(base_url=docker_base_url)
 
+    @staticmethod
+    def __get_swarmkeeper_flag_from_label(service: Service) -> str:
+        labels: dict = service.attrs.get("Spec")["Labels"]
+        if "swarmkeeper.enable" in labels:
+            if labels["swarmkeeper.enable"] == "true":
+                return "Y"
+            else:
+                return "N"
+        else:
+            return "X"
+
     def get_image_digest_registry(self, image_tag: str) -> str:
         image_registry_data = self.__client.images.get_registry_data(image_tag)
         return image_registry_data.id
@@ -19,9 +33,10 @@ class DockerApi:
         service_dict: dict = {}
         for service in self.__client.services.list():
             if service.id in service_id_set:
-                image_tag: str = service.attrs.get("Spec")["Labels"]["com.docker.stack.image"]
                 service_dict[service.id] = {"service_name": service.name,
-                                            "image_tag": image_tag}
+                                            "image_tag": service.attrs.get("Spec")["Labels"]["com.docker.stack.image"],
+                                            "image_digest": service.attrs.get("Spec")["TaskTemplate"]["ContainerSpec"]["Image"],
+                                            "process_flag": self.__get_swarmkeeper_flag_from_label(service)}
         return service_dict
 
     def update_service_force(self, id_service: str):
--- docker_service/docker_service_maintenance.py
+++ docker_service/docker_service_maintenance.py
@@ -10,34 +10,39 @@ class DockerServiceMaintenance:
         self.__docker_api: DockerApi = docker_api
         self.__task_queue: Queue = task_queue
         self.__service_ignore_list: list = config.get("DEFAULT", "service_ignore_list").split(" ")
-        self.__namespace_ignore_list: list = config.get("DEFAULT", "namespace_ignore_list").split(" ")
 
     @staticmethod
     def __get_distinct_value_list(dictionary_list: list[dict], key) -> set:
         return {dictionary[key] for dictionary in dictionary_list if key in dictionary and dictionary[key]}
 
-    def __process_service_container(self, container: dict, service: dict):
-        image_digest_repository: str = self.__docker_api.get_image_digest_registry(service["image_tag"])
-        if image_digest_repository in container["image_digest_list"]:
-            logging.info(f"Container {container['name']} [{container['service_name']}] is using up to date image")
+    def __can_process_service_container(self, container: dict, service: dict) -> bool:
+        if service["process_flag"] == "N":
+            logging.info(f"Skip processing of container {container['name']} [Service {container['service_name']}], because it was disabled by service label")
+            return False
+        elif self.__service_ignore_list and container["service_name"] in self.__service_ignore_list:
+            logging.info(f"Skip processing of container {container['name']} [Service {container['service_name']}], because it belongs to ignored service")
+            return False
         else:
-            logging.info(f"Container {container['name']} [{container['service_name']}] is using outdated image. Update of service is in progress ...")
-            if self.__docker_api.update_service_force(container["id_service"]):
-                logging.info(f"Update of service {service['service_name']} successfully finished")
+            return True
+
+    def __process_service_container(self, container: dict, service: dict):
+        if self.__can_process_service_container(container, service):
+            image_digest_repository: str = self.__docker_api.get_image_digest_registry(service["image_tag"])
+            if image_digest_repository in container["image_digest_list"]:
+                logging.info(f"Container {container['name']} [{container['service_name']}] is using up to date image")
             else:
-                logging.error(f"Update of service {service['service_name']} failed!")
+                logging.info(f"Container {container['name']} [{container['service_name']}] is using outdated image. Update of service is in progress ...")
+                if self.__docker_api.update_service_force(container["id_service"]):
+                    logging.info(f"Update of service {service['service_name']} successfully finished")
+                else:
+                    logging.error(f"Update of service {service['service_name']} failed!")
 
     def __process_container_list(self, container_list: list[dict]):
         service_id_list = self.__get_distinct_value_list(container_list, "id_service")
         service_dict = self.__docker_api.get_service_list(service_id_list)
         for container in container_list:
             if container["id_service"]:
-                if self.__service_ignore_list and container["service_name"] in self.__service_ignore_list:
-                    logging.info(f"Skip processing of container {container['name']} [{container['service_name']}], because it belongs to ignored service")
-                elif self.__namespace_ignore_list and container["stack_namespace"] in self.__namespace_ignore_list:
-                    logging.info(f"Skip processing of container {container['name']} [{container['service_name']}], because it belongs to ignored stack namespace {container['stack_namespace']}")
-                else:
-                    self.__process_service_container(container, service_dict[container["id_service"]])
+                self.__process_service_container(container, service_dict[container["id_service"]])
 
     def process_queue(self):
         if self.__task_queue.qsize() > 0:
